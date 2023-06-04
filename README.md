39906a743242f9253162df146531a06fe4dd2312
Pedro Monteiro <pedro.monteiro@wansoft.com.br>
2023-06-04 11:16:20 -0300

    Initial configuration

---
+++ README.md
@@ -0,0 +1,26 @@
+# ESLint config
+
+## Whats included?
+
+- Standard config base;
+- React plugin;
+- React Hooks plugin;
+- JSX a11y plugin;
+- Prettier;
+
+## Setup
+
+1. Install the dependencies
+```
+npm i -D eslint @pemonter/eslint-config
+```
+
+2. Create a `.eslintrc.json` file extending the config:
+```
+{
+  "extends": "@pemonter/eslint-config/react"
+  // "extends": "@pemonter/eslint-config/node"
+}
+```
+
+> You can also use a `.eslintrc.js` instead of JSON if you prefer.
---
+++ react.js
@@ -0,0 +1,135 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'standard',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': ['error', {
+      'printWidth': 80,
+      'tabWidth': 2,
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': true,
+      'endOfLine': 'auto',
+    }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'camelcase': 'off',
+    'import/no-unresolved': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': [
+          'PascalCase'
+        ],
+        'custom': {
+          'regex': '^I[A-Z]',
+          'match': true
+        }
+      }
+    ],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off',
+    'no-console': 'off',
+    'no-useless-constructor': 'off',
+    'no-empty-function': 'off',
+    'lines-between-class-members': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'ts': 'never',
+        'tsx': 'never'
+      }
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        'newlinesBetween': 'always',
+        'groups': [
+          'module',
+          '/^@shared/',
+          [
+            'parent',
+            'sibling',
+            'index'
+          ]
+        ],
+        'alphabetize': {
+          'order': 'asc',
+          'ignoreCase': true
+        }
+      }
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        'devDependencies': [
+          '**/*.spec.js'
+        ]
+      }
+    ],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        'extensions': [
+          '.jsx',
+          '.tsx'
+        ]
+      }
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error'
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  }
+}
